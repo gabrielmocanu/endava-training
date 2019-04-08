8421ee489d8bc5e85f1eb3e3035a49e74a36fcc1
Mocanu Gabriel <gabimocanu98@gmail.com>
2019-04-08 19:56:25 +0300

    new

---
+++ soj1-Exemplu1/src/main/java/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import config.ProjectConfig;
+import objects.Cat;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        try(AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class)) {
+            Cat c = context.getBean(Cat.class);
+
+            System.out.println(c.name);
+        }
+    }
+}
